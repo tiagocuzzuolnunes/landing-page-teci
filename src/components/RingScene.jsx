c0a8518d08f19e7190eef73797415c952b1d55bc
tiagocuzzuolnunes <tiagocuzzuolnunes@gmail.com>
2025-03-19 10:44:17 -0300

    alterando modelo 3d

--- src/components/RingScene.jsx
+++ src/components/RingScene.jsx
@@ -8,10 +8,10 @@ function SetCamera() {
     const { camera, gl } = useThree();
 
     useEffect(() => {
-        // Definindo a posição da câmera de forma fixa
-        camera.position.set(1, 0, 0); // Ajuste conforme necessário para o seu modelo
-        camera.lookAt(0, 0, 0); // Garante que a câmera esteja olhando para o centro
-        gl.domElement.style.touchAction = 'none'; // Desabilita o movimento da câmera ao clicar
+
+        camera.position.set(1, 0, 0);
+        camera.lookAt(0, 0, 0);
+        gl.domElement.style.touchAction = 'none';
     }, [camera, gl]);
 
     return null;
@@ -21,17 +21,8 @@ function RingModel() {
     const { scene } = useGLTF('/anelFinal4.glb');
     const groupRef = useRef();
 
-    // Garantir que a rotação aconteça em torno do centro do objeto sem translação
-    // useFrame(() => {
-    //     if (groupRef.current) {
-    //         // Aplicar rotação apenas no eixo Y sem afetar a posição
-    //         groupRef.current.rotation.y += 0.05;
-    //     }
-    // });
-
     return (
         <group ref={groupRef}>
-            {/* O modelo GLTF é carregado dentro do group */}
             <primitive object={scene} position={[0, 0, 0]} scale={1} />
         </group>
     );
@@ -40,24 +31,18 @@ function RingModel() {
 export default function RingScene() {
     return (
         <Canvas className='pt-16'>
-            {/* Luz ambiente e luz direcional */}
             <ambientLight intensity={10} />
             <ambientLight intensity={10} />
             <ambientLight intensity={10} />
             <directionalLight position={[5, 5, 5]} intensity={10} />
             <directionalLight position={[10, 10, 10]} intensity={10} />
-
-            {/* Carregar o modelo GLB */}
             <RingModel />
-
             <SetCamera />
-
-            {/* OrbitControls para rotação horizontal apenas */}
             <OrbitControls
                 enableZoom={true}
                 enablePan={false}
-                enableRotate={true} // Desabilitar rotação da câmera
-                target={[0, 0, 0]}  // Gira em torno do centro do objeto
+                enableRotate={true}
+                target={[0, 0, 0]}
             />
             <Environment preset="sunset" />
         </Canvas>
